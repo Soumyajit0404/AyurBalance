31dd54d8cbcc14555e7cb1749f4ef705f764b139
GAWD PARKER <gawdparker@gmail.com>
2025-09-27 05:57:06 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,20 @@
-import { LoginClient } from "./client";
+"use client";
+
+import dynamic from 'next/dynamic'
+import { Skeleton } from '@/components/ui/skeleton'
+
+const LoginClient = dynamic(() => import('./client').then(mod => mod.LoginClient), {
+  ssr: false,
+  loading: () => (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-[400px] space-y-4">
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-64 w-full" />
+      </div>
+    </div>
+  ),
+})
 
 export default function LoginPage() {
-  return <LoginClient />;
+  return <LoginClient />
 }
